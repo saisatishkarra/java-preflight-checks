088558e8c533742b9b3ac8de1c9d19ef4238b864
sai satish karra <saisatish.karra@dataguise.com>
2020-02-13 15:34:18 -0800

    test-2

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -25,12 +25,12 @@ fi
 # pmd java
 echo "Running PMD"
 mkdir pmd
-/usr/local/lib/pmd/bin/run.sh pmd -d  ${SRC_DIR} -f xml -rulesets java-quickstart -l java -r pmd\pmd.xml
+/usr/local/lib/pmd/bin/run.sh pmd -d  ${SRC_DIR} -f xml -rulesets java-quickstart -l java -r pmd/pmd.xml
 
 # checkstyle
 echo "Running Checkstyle"
 mkdir checkstyle-result
-java -jar /usr/local/lib/checkstyle.jar ${SRC_DIR} -f xml -c /google_checks.xml -o checkstyle-result\checkstyle-result.xml
+java -jar /usr/local/lib/checkstyle.jar ${SRC_DIR} -f xml -c /google_checks.xml -o checkstyle-result/checkstyle-result.xml
 #echo "Convertin Checkstyle xml to html report using xsl"
 #xsltproc -o cs-report.xml checkstyle-frames.xsl ${CHECK_STYLE_REPORT_FILE}.html
 
